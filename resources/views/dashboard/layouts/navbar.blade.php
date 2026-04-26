3c72422c17b42d30d8437fc28f7452edcda982be
Abdallh ELzayat <abdallhelzayat.dev@gmail.com>
2026-04-26 14:05:12 +0300

    Update navbar user dropdown to link to profile edit route instead of static page. Improve code formatting for better readability.

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -289,13 +289,12 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="{{ auth()->user()->profileImageUrl() }}" alt
-                            class="h-auto rounded-circle" />
+                        <img src="{{ auth()->user()->profileImageUrl() }}" alt class="h-auto rounded-circle" />
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li>
-                        <a class="dropdown-item" href="pages-account-settings-account.html">
+                        <a class="dropdown-item" href="{{ route('admin.profile.edit') }}">
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
